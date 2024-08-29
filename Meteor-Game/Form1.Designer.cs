f9490f639e900ef129be1f08711ab1e3de1ae3af
TsubasaKato2024 <uni202406_06@telework.to>
2024-08-29 11:00:53 +0900

    画像を追加し、重ねて表示させる方法

--- Meteor-Game/Form1.Designer.cs
+++ Meteor-Game/Form1.Designer.cs
@@ -28,13 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pBG = new System.Windows.Forms.PictureBox();
+            this.pMeteor = new System.Windows.Forms.PictureBox();
+            this.pBase = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pBG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pMeteor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBase)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pBG
+            // 
+            this.pBG.Image = global::Meteor_Game.Properties.Resources.背景_宇宙_;
+            this.pBG.Location = new System.Drawing.Point(12, 12);
+            this.pBG.Name = "pBG";
+            this.pBG.Size = new System.Drawing.Size(170, 124);
+            this.pBG.TabIndex = 1;
+            this.pBG.TabStop = false;
+            // 
+            // pMeteor
+            // 
+            this.pMeteor.Image = global::Meteor_Game.Properties.Resources.隕石;
+            this.pMeteor.Location = new System.Drawing.Point(58, 34);
+            this.pMeteor.Name = "pMeteor";
+            this.pMeteor.Size = new System.Drawing.Size(84, 76);
+            this.pMeteor.TabIndex = 0;
+            this.pMeteor.TabStop = false;
+            // 
+            // pBase
+            // 
+            this.pBase.Location = new System.Drawing.Point(231, 12);
+            this.pBase.Name = "pBase";
+            this.pBase.Size = new System.Drawing.Size(219, 249);
+            this.pBase.TabIndex = 2;
+            this.pBase.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(462, 273);
+            this.Controls.Add(this.pBase);
+            this.Controls.Add(this.pMeteor);
+            this.Controls.Add(this.pBG);
+            this.Name = "Form1";
+            this.Text = "メテオゲーム";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pMeteor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBase)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pMeteor;
+        private System.Windows.Forms.PictureBox pBG;
+        private System.Windows.Forms.PictureBox pBase;
     }
 }
 
